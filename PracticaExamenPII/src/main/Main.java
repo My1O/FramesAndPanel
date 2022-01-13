c54c2369f1fb1ede8484dfd5ebff0ab4747eb923
Camilo <castroh24@gmail.com>
2022-01-12 22:38:17 -0600

    cuarto cambio

--- PracticaExamenPII/src/main/Main.java
+++ PracticaExamenPII/src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		System.out.println("Cambio ingresado despues del primero commit / push");
 		//3rd change from gitHub
 		System.out.println("cambio realizado en gitHub");
-
+		
 	}
 
 }
--- PracticaExamenPII/src/view/panel/LaminaDibujo.java
+++ PracticaExamenPII/src/view/panel/LaminaDibujo.java
@@ -1,6 +1,7 @@
 package view.panel;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
@@ -21,12 +22,20 @@ public class LaminaDibujo extends JPanel{
 		
 		Graphics2D g2 = (Graphics2D) g;
 		Rectangle2D rectangulo = new Rectangle2D.Double(100,100,200,150);
-		g2.setColor(Color.blue);
+		g2.setColor(Color.red);
 		g2.fill(rectangulo);
 		
 		Ellipse2D elipse = new Ellipse2D.Double();
 		elipse.setFrame(rectangulo);
 		g2.setPaint(new Color(20,200,150));
 		g2.fill(elipse);
+		//creando fuente
+		Font miFuente = new Font("Arial", Font.BOLD,26);
+		g2.setFont(miFuente);
+		g2.drawString("Camilo", 160, 80);
+		g2.setFont(new Font("Arial",Font.ITALIC,14));
+		g2.setColor(new Color(128,90,50));
+		g2.drawString("Ejemplo Java", 160, 180);
+		
 	}
 }
